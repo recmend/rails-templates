cb38fb2f044dfa8c7e6a25eec94c00aaeb96133d
Arun Agrahri <agrahri.arun@gmail.com>
2009-06-22 21:43:02 -0500

    First Commit

---
+++ authentication.rb
@@ -0,0 +1,8 @@
+#Load base template work here
+load_template "http://github.com/recmend/rails-templates/raw/master/base.rb"
+ 
+name = ask("What do you want a user to be called?")
+generate :nifty_authentication, name
+rake "db:migrate"
+ 
+git :add => ".", :commit => "-m 'adding authentication'"
